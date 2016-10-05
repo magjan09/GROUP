1a452a2a0541c2443dc2a87a07fc87b9c2bcffa2
magja_000 <magjan.amangeldi@mail.ru>
2016-10-05 16:11:12 +0600

    Я добавил что-то

---
+++ app/src/main/java/github/com/group/maga.java
@@ -0,0 +1,11 @@
+package github.com.group;
+
+/**
+ * Created by magja_000 on 05.10.2016.
+ */
+public class maga {
+    public static void main(String[] args) {
+        System.out.println("Maga");
+    }
+
+}
